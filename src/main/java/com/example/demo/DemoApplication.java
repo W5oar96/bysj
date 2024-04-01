fe6e29bf670d6d178b18a0acd77cb1278b1c39c7
W5oar96 <imborg@qq.com>
2024-04-01 10:38:23 +0800

    my init commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//项目启动
+@SpringBootApplication
+public class DemoApplication {
+//主方法
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
